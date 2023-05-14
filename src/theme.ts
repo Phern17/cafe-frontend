3301c60b76cab0af2c3c54bdf55b1bdc5c75478d
Phern <mphern17@gmail.com>
2023-05-14 22:47:57 +0800

    first page added

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#058892',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: 'Droid Sans',
+  },
+};
